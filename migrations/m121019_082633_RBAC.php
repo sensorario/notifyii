0462a81d96cd8208c6ca695cae1010d22cdf72c9
Simone Gentili <sensorario@gmail.com>
2012-10-19 10:59:18 +0200

    Added new migration for RBAC system [fixed #19]

---
+++ migrations/m121019_082633_RBAC.php
@@ -0,0 +1,45 @@
+<?php
+
+class m121019_082633_RBAC extends CDbMigration
+{
+    public function up()
+    {
+        try {
+            $this->createTable('AuthItem', array(
+                'name' => 'string',
+                'type' => 'integer',
+                'description' => 'text',
+                'data' => 'text',
+            ));
+        } catch (Exception $e) {
+            // Nothing to do if table already exists
+        }
+
+        try {
+            $this->createTable('AuthItemChild', array(
+                'parent' => 'string',
+                'child' => 'string',
+            ));
+        } catch (Exception $e) {
+            // Nothing to do if table already exists
+        }
+
+        try {
+            $this->createTable('AuthAssignment', array(
+                'itemname' => 'string',
+                'userid' => 'string',
+                'bizrule' => 'text',
+                'date' => 'text'
+            ));
+        } catch (Exception $e) {
+            // Nothing to do if table already exists
+        }
+    }
+
+    public function down()
+    {
+        echo "m121019_082633_RBAC does not support migration down.\n";
+	return false;
+    }
+
+}
